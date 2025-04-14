1bdc26c285257b528d260e35e22279437f05a19a
Afranio Alcantara <q.aafranio.alcantara@gmail.com>
2025-04-14 13:19:31 -0300

    adicionando o projeto
    subida do projeto como um todo antes das novas atualizações

---
+++ data/characters.js
@@ -0,0 +1,406 @@
+// data/characters.js
+module.exports = [
+
+        {
+          "id": 1,
+          "nome": "Monkey D. Luffy",
+          "anime": "One Piece",
+          "idade": 19,
+          "altura": "1,74 m",
+          "curiosidade": "Luffy possui um corpo elástico devido à Gomu-Gomu no Mi."
+        },
+        {
+          "id": 2,
+          "nome": "Goku (Son Goku)",
+          "anime": "Dragon Ball",
+          "idade": 43,
+          "altura": "1,75 m",
+          "curiosidade": "Goku é baseado em Sun Wukong, o Rei Macaco do romance chinês 'Jornada ao Oeste'."
+        },
+        {
+          "id": 3,
+          "nome": "Naruto Uzumaki",
+          "anime": "Naruto",
+          "idade": 17,
+          "altura": "1,80 m",
+          "curiosidade": "Naruto sonha em se tornar Hokage para ser reconhecido por todos."
+        },
+        {
+          "id": 4,
+          "nome": "Roronoa Zoro",
+          "anime": "One Piece",
+          "idade": 21,
+          "altura": "1,81 m",
+          "curiosidade": "Zoro é conhecido por sua técnica de combate com três espadas, chamada Santoryu."
+        },
+        {
+          "id": 5,
+          "nome": "Itachi Uchiha",
+          "anime": "Naruto",
+          "idade": 21,
+          "altura": "1,78 m",
+          "curiosidade": "Itachi eliminou todo o clã Uchiha, exceto seu irmão Sasuke."
+        },
+        {
+          "id": 6,
+          "nome": "Ichigo Kurosaki",
+          "anime": "Bleach",
+          "idade": 15,
+          "altura": "1,74 m",
+          "curiosidade": "Ichigo tem o poder de ver espíritos desde pequeno."
+        },
+        {
+          "id": 7,
+          "nome": "Lelouch vi Britannia",
+          "anime": "Code Geass",
+          "idade": 17,
+          "altura": "1,80 m",
+          "curiosidade": "Lelouch possui o Geass, que lhe permite controlar a mente de qualquer pessoa."
+        },
+        {
+          "id": 8,
+          "nome": "Saitama",
+          "anime": "One Punch Man",
+          "idade": 25,
+          "altura": "1,75 m",
+          "curiosidade": "Saitama é um herói que derrota qualquer inimigo com apenas um soco."
+        },
+        {
+          "id": 9,
+          "nome": "Eren Yeager",
+          "anime": "Attack on Titan",
+          "idade": 15,
+          "altura": "1,70 m",
+          "curiosidade": "Eren se torna um titã após ser atacado por um titã ancestral."
+        },
+        {
+          "id": 10,
+          "nome": "Sasuke Uchiha",
+          "anime": "Naruto",
+          "idade": 17,
+          "altura": "1,72 m",
+          "curiosidade": "Sasuke é o último sobrevivente do clã Uchiha, depois do massacre de seu clã."
+        },
+        {
+          "id": 11,
+          "nome": "Hinata Hyuga",
+          "anime": "Naruto",
+          "idade": 16,
+          "altura": "1,60 m",
+          "curiosidade": "Hinata possui o Byakugan, uma habilidade ocular única de seu clã."
+        },
+        {
+          "id": 12,
+          "nome": "Kagome Higurashi",
+          "anime": "Inuyasha",
+          "idade": 15,
+          "altura": "1,58 m",
+          "curiosidade": "Kagome é a reencarnação de Kikyo, uma sacerdotisa do passado."
+        },
+        {
+          "id": 13,
+          "nome": "Mikasa Ackerman",
+          "anime": "Attack on Titan",
+          "idade": 15,
+          "altura": "1,69 m",
+          "curiosidade": "Mikasa é a irmã adotiva de Eren Yeager e uma das melhores combatentes da Tropa de Exploração."
+        },
+        {
+          "id": 14,
+          "nome": "L Lawliet",
+          "anime": "Death Note",
+          "idade": 24,
+          "altura": "1,80 m",
+          "curiosidade": "L é um detetive excêntrico, conhecido por resolver casos aparentemente impossíveis."
+        },
+        {
+          "id": 15,
+          "nome": "Natsu Dragneel",
+          "anime": "Fairy Tail",
+          "idade": 19,
+          "altura": "1,70 m",
+          "curiosidade": "Natsu é um Dragon Slayer, com o poder de usar o fogo de Igneel, um dragão."
+        },
+        {
+          "id": 16,
+          "nome": "Erza Scarlet",
+          "anime": "Fairy Tail",
+          "idade": 19,
+          "altura": "1,70 m",
+          "curiosidade": "Erza tem a habilidade de equipar-se com diferentes armaduras e armas mágicas."
+        },
+        {
+          "id": 17,
+          "nome": "Shoto Todoroki",
+          "anime": "My Hero Academia",
+          "idade": 15,
+          "altura": "1,76 m",
+          "curiosidade": "Shoto tem o poder de controlar tanto fogo quanto gelo."
+        },
+        {
+          "id": 18,
+          "nome": "Deku (Izuku Midoriya)",
+          "anime": "My Hero Academia",
+          "idade": 15,
+          "altura": "1,60 m",
+          "curiosidade": "Deku é o herói principal da história e possui o poder 'One For All'."
+        },
+        {
+          "id": 19,
+          "nome": "Kamina",
+          "anime": "Tengen Toppa Gurren Lagann",
+          "idade": 14,
+          "altura": "1,75 m",
+          "curiosidade": "Kamina é um dos principais personagens do anime, conhecido por sua personalidade ousada e corajosa."
+        },
+        {
+          "id": 20,
+          "nome": "Yusuke Urameshi",
+          "anime": "Yu Yu Hakusho",
+          "idade": 14,
+          "altura": "1,75 m",
+          "curiosidade": "Yusuke começa como um delinquente, mas depois se torna um detetive espiritual."
+        },
+        {
+          "id": 21,
+          "nome": "Edward Elric",
+          "anime": "Fullmetal Alchemist",
+          "idade": 15,
+          "altura": "1,65 m",
+          "curiosidade": "Edward é o alquimista de aço e perdeu seu braço e perna devido a um experimento alquímico."
+        },
+        {
+          "id": 22,
+          "nome": "Roy Mustang",
+          "anime": "Fullmetal Alchemist",
+          "idade": 29,
+          "altura": "1,76 m",
+          "curiosidade": "Roy é o Alquimista de Fogo e possui um grande poder militar."
+        },
+        {
+          "id": 23,
+          "nome": "Saber (Artoria Pendragon)",
+          "anime": "Fate/Stay Night",
+          "idade": "De 20 a 30 anos",
+          "altura": "1,55 m",
+          "curiosidade": "Saber é a reencarnação de Artoria, a lendária Rainha da Grã-Bretanha."
+        },
+        {
+          "id": 24,
+          "nome": "Yuno Gasai",
+          "anime": "Future Diary",
+          "idade": 14,
+          "altura": "1,52 m",
+          "curiosidade": "Yuno é obcecada por Yukiteru e faz tudo para garantir sua sobrevivência no jogo dos diários."
+        },
+        {
+          "id": 25,
+          "nome": "Homura Akemi",
+          "anime": "Puella Magi Madoka Magica",
+          "idade": 14,
+          "altura": "1,58 m",
+          "curiosidade": "Homura tem a habilidade de manipular o tempo, fazendo-a reviver repetidamente os mesmos eventos."
+        },
+        {
+            "id": 26,
+            "nome": "Kenshin Himura",
+            "anime": "Rurouni Kenshin",
+            "idade": 28,
+            "altura": "1,63 m",
+            "curiosidade": "Kenshin é um ex-assassino que jurou nunca mais matar, usando uma espada com a lâmina voltada para trás."
+          },
+          {
+            "id": 27,
+            "nome": "Luffy D. Monkey",
+            "anime": "One Piece",
+            "idade": 19,
+            "altura": "1,74 m",
+            "curiosidade": "Luffy tem o sonho de se tornar o Rei dos Piratas e explorar o One Piece, o maior tesouro do mundo."
+          },
+          {
+            "id": 28,
+            "nome": "Moka Akashiya",
+            "anime": "Rosario + Vampire",
+            "idade": 15,
+            "altura": "1,65 m",
+            "curiosidade": "Moka é uma vampira com duas personalidades: uma doce e humana, e outra poderosa e perigosa."
+          },
+          {
+            "id": 29,
+            "nome": "Inuyasha",
+            "anime": "Inuyasha",
+            "idade": 200,
+            "altura": "1,70 m",
+            "curiosidade": "Inuyasha é um meio-humano e meio-demônio, buscando a Jóia de Quatro Almas Apesar de ter mais de 200 anos aparenta ter apenas 16."
+          },
+          {
+            "id": 30,
+            "nome": "Kamina",
+            "anime": "Tengen Toppa Gurren Lagann",
+            "idade": 14,
+            "altura": "1,75 m",
+            "curiosidade": "Kamina é o irmão mais velho de Simon e possui um grande espírito de liderança."
+          },
+          {
+            "id": 31,
+            "nome": "Yugi Mutou",
+            "anime": "Yu-Gi-Oh!",
+            "idade": 16,
+            "altura": "1,58 m",
+            "curiosidade": "Yugi é um especialista em jogos e herda um antigo espírito de faraó com poderes místicos."
+          },
+          {
+            "id": 32,
+            "nome": "Rin Tohsaka",
+            "anime": "Fate/Stay Night",
+            "idade": 17,
+            "altura": "1,59 m",
+            "curiosidade": "Rin é uma das principais heroínas de Fate/Stay Night e tem uma habilidade mágica impressionante."
+          },
+          {
+            "id": 33,
+            "nome": "Vash the Stampede",
+            "anime": "Trigun",
+            "idade": 131,
+            "altura": "1,84 m",
+            "curiosidade": "Vash é conhecido por sua incrível habilidade com armas e por ser um pacifista, apesar de sua fama de destruidor."
+          },
+          {
+            "id": 34,
+            "nome": "Nico Robin",
+            "anime": "One Piece",
+            "idade": 30,
+            "altura": "1,88 m",
+            "curiosidade": "Robin é uma arqueóloga com a habilidade de fazer crescer partes do corpo em qualquer superfície."
+          },
+          {
+            "id": 35,
+            "nome": "Gintoki Sakata",
+            "anime": "Gintama",
+            "idade": 27,
+            "altura": "1,77 m",
+            "curiosidade": "Gintoki é um samurai preguiçoso e rebelde, mas com um coração nobre e habilidades excepcionais."
+          },
+          {
+            "id": 36,
+            "nome": "Kyo Sohma",
+            "anime": "Fruits Basket",
+            "idade": 16,
+            "altura": "1,75 m",
+            "curiosidade": "Kyo é um dos membros da família Sohma e se transforma em um gato quando é abraçado por alguém do sexo oposto."
+          },
+          {
+            "id": 37,
+            "nome": "Shinya Kogami",
+            "anime": "Psycho-Pass",
+            "idade": 29,
+            "altura": "1,82 m",
+            "curiosidade": "Kogami é um ex-oficial de polícia que caça criminosos com seu próprio senso de justiça."
+          },
+          {
+            "id": 38,
+            "nome": "Todoroki Shoto",
+            "anime": "My Hero Academia",
+            "idade": 15,
+            "altura": "1,76 m",
+            "curiosidade": "Shoto pode controlar tanto o gelo quanto o fogo devido ao poder de seus pais."
+          },
+          {
+            "id": 39,
+            "nome": "Asuka Langley Soryu",
+            "anime": "Neon Genesis Evangelion",
+            "idade": 14,
+            "altura": "1,57 m",
+            "curiosidade": "Asuka é uma pilota de EVA altamente talentosa, mas com uma personalidade explosiva."
+          },
+          {
+            "id": 40,
+            "nome": "Kagome Higurashi",
+            "anime": "Inuyasha",
+            "idade": 15,
+            "altura": "1,58 m",
+            "curiosidade": "Kagome tem uma ligação mística com a Jóia de Quatro Almas, que causa grandes conflitos no enredo."
+          },
+          {
+            "id": 41,
+            "nome": "Saber (Artoria Pendragon)",
+            "anime": "Fate/Stay Night",
+            "idade": "De 20 a 30 anos",
+            "altura": "1,55 m",
+            "curiosidade": "Saber é a reencarnação de Artoria, a lendária Rainha da Grã-Bretanha."
+          },
+          {
+            "id": 42,
+            "nome": "Rikuto Aizawa",
+            "anime": "Alderamin on the Sky",
+            "idade": 17,
+            "altura": "1,81 m",
+            "curiosidade": "Rikuto é um estrategista militar altamente habilidoso e um dos principais personagens da série."
+          },
+          {
+            "id": 43,
+            "nome": "Saber (Altria Pendragon)",
+            "anime": "Fate/Stay Night",
+            "idade": "De 20 a 30 anos",
+            "altura": "1,55 m",
+            "curiosidade": "Saber é a reencarnação de Artoria, a lendária Rainha da Grã-Bretanha."
+          },
+          {
+            "id": 44,
+            "nome": "Nyaruko",
+            "anime": "Haiyore! Nyaruko-san",
+            "idade": "Desconhecida",
+            "altura": "1,60 m",
+            "curiosidade": "Nyaruko é uma deusa do caos e luta contra seres malignos de outras dimensões."
+          },
+          {
+            "id": 45,
+            "nome": "Kamina",
+            "anime": "Tengen Toppa Gurren Lagann",
+            "idade": "14",
+            "altura": "1,75 m",
+            "curiosidade": "Kamina é o irmão mais velho de Simon e possui um grande espírito de liderança."
+          },
+          {
+            "id": 46,
+            "nome": "Haruhi Fujioka",
+            "anime": "Ouran High School Host Club",
+            "idade": 15,
+            "altura": "1,63 m",
+            "curiosidade": "Haruhi é uma garota que se passa por menino para conseguir uma bolsa de estudos na escola de elite."
+          },
+          {
+            "id": 47,
+            "nome": "Kou Mabuchi",
+            "anime": "Ao Haru Ride",
+            "idade": 16,
+            "altura": "1,72 m",
+            "curiosidade": "Kou é um personagem que tem dificuldades em se expressar, mas guarda um passado cheio de emoções."
+          },
+          {
+            "id": 48,
+            "nome": "Hachiman Hikigaya",
+            "anime": "Oregairu",
+            "idade": 16,
+            "altura": "1,80 m",
+            "curiosidade": "Hachiman tem uma visão cínica da vida e da sociedade, o que o torna um dos personagens mais complexos."
+          },
+          {
+            "id": 49,
+            "nome": "Mami Tomoe",
+            "anime": "Puella Magi Madoka Magica",
+            "idade": 14,
+            "altura": "1,62 m",
+            "curiosidade": "Mami é uma das Puella Magi mais experientes, com uma habilidade impressionante de combate."
+          },
+          {
+            "id": 50,
+            "nome": "Sakura Haruno",
+            "anime": "Naruto",
+            "idade": 16,
+            "altura": "1,60 m",
+            "curiosidade": "Sakura tem uma força física impressionante e é uma das ninjas mais habilidosas de sua geração."
+          }, 
+      
+  ];
+  
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const characters = require('./data/characters');
+
+// Swagger
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Rotas
+app.get('/api/characters', (req, res) => {
+  res.json(characters);
+});
+
+app.get('/api/characters/:id', (req, res) => {
+  const characterId = parseInt(req.params.id);
+  const character = characters.find(c => c.id === characterId);
+
+  if (!character) {
+    return res.status(404).json({ mensagem: "Personagem não encontrado" });
+  }
+
+  res.json(character);
+});
+
+app.listen(port, () => {
+  console.log(`API de personagens de anime rodando em http://localhost:${port}`);
+  console.log(`Swagger disponível em http://localhost:${port}/api-docs`);
+});
